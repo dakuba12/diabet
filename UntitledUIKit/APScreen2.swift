9b4d88ee77e321d9e4d668065f56c24916313390
alexey <macbook@mac-mini-alexey>
2016-12-11 23:23:48 +0200

    Initial commit

---
+++ UntitledUIKit/APScreen2.swift
@@ -0,0 +1,16 @@
+//
+//  APScreen2.swift
+//  Untitled
+//
+//  Created by alexeyweb39@gmail.com on 11/12/2016, using AnimaApp.com, under MIT license.
+//  Copyright © 2016 Company Name. All rights reserved.
+//
+
+import UIKit
+
+public class APScreen2 : ANViewController {
+    
+
+    
+
+}
---
+++ UntitledUIKit/UntitledUIKit.h
@@ -0,0 +1,39 @@
+//
+//  UntitledUIKit.h
+//  UntitledUIKit
+//
+//  Created by alexeyweb39@gmail.com on 11/12/2016, using AnimaApp.com, under MIT license.
+//  Copyright © 2016 Company Name. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UntitledUIKit.
+FOUNDATION_EXPORT double UntitledUIKitVersionNumber;
+
+//! Project version string for UntitledUIKit.
+FOUNDATION_EXPORT const unsigned char UntitledUIKitVersionString[];
+
+#import <UntitledUIKit/UIView+AnimaConstraints.h>
+#import <UntitledUIKit/UIWebView+Anima.h>
+#import <UntitledUIKit/ANDataList.h>
+#import <UntitledUIKit/ANProtocols.h>
+#import <UntitledUIKit/UIImageView+Anima.h>
+#import <UntitledUIKit/AnimaSDK.h>
+#import <UntitledUIKit/NSString+Anima.h>
+#import <UntitledUIKit/ANClassMapping.h>
+#import <UntitledUIKit/MKMapView+Anima.h>
+#import <UntitledUIKit/ANTabBarController.h>
+#import <UntitledUIKit/ANVideoPlayer.h>
+#import <UntitledUIKit/ANViewController.h>
+#import <UntitledUIKit/NSDictionary+Anima.h>
+#import <UntitledUIKit/ANCSVParser.h>
+#import <UntitledUIKit/UIView+Anima.h>
+#import <UntitledUIKit/ANLabel.h>
+#import <UntitledUIKit/ANNavigationController.h>
+#import <UntitledUIKit/Anima-Bridging-Header.h>
+#import <UntitledUIKit/ANMacros.h>
+#import <UntitledUIKit/ANTextField.h>
+#import <UntitledUIKit/ANTheme.h>
+#import <UntitledUIKit/ANGradientView.h>
+#import <UntitledUIKit/ANRSSParser.h>
